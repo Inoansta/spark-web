09a8b24d6b7292bd77c2a618406ca1979d78aaed
kim <perfumeplaylist@gmail.com>
2025-01-11 19:13:05 +0900

    chore: eslint & prettier 설정

---
+++ spark-web/eslint.config.mjs
@@ -0,0 +1,88 @@
+import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  eslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: { ...globals.browser },
+    },
+    rules: {
+      eqeqeq: 'error',
+      curly: ['warn', 'multi-line', 'consistent'],
+      'sort-imports': [
+        'error',
+        {
+          ignoreDeclarationSort: true,
+        },
+      ],
+      'import/no-unresolved': ['error', { commonjs: true, amd: true }],
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-duplicates': 'error',
+      'import/extensions': ['error', 'ignorePackages'],
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+          ],
+          'newlines-between': 'never',
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'react-compiler/react-compiler': 'warn',
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+
+  {
+    files: ['*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+);
