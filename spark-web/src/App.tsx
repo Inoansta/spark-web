a5917c530b057130d48e169d88aae448b7bcb49f
Paul Yang <sea5122916@gmail.com>
2025-01-18 19:40:23 +0900

    feat: tailwind config 수정

--- spark-web/src/App.tsx
+++ spark-web/src/App.tsx
@@ -20,6 +20,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
+			<div class={"text-black"}>Hello</div>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
