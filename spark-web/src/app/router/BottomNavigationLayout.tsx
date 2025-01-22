3d086b60f6bdb88e69ea9ecff7517355c7cdd6aa
perfumeplaylist <perfumeplaylist@gmail.com>
2025-01-22 21:35:51 +0900

    feat: 페이지 정의 및 layout 정의

---
+++ spark-web/src/app/router/BottomNavigationLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router';
+
+export default function BottomNavigationLayout() {
+  return (
+    <div>
+      Layout
+      <Outlet />
+    </div>
+  );
+}
---
+++ spark-web/src/app/router/PageLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router';
+
+export default function PageLayout() {
+  return (
+    <div>
+      PageLayout
+      <Outlet />
+    </div>
+  );
+}
