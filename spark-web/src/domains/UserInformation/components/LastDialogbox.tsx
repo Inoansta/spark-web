0e225bf310ccf887635def9c9ed53a5f7193bbab
Paul Yang <sea5122916@gmail.com>
2025-01-23 15:57:08 +0900

    feat: 질문자측 대화창 구현

---
+++ spark-web/src/domains/UserInformation/components/LastDialogbox.tsx
@@ -0,0 +1,19 @@
+interface LastDialogbox {
+	questions: string;
+}
+
+function LastDialogbox({ questions }: LastDialogbox) {
+	return (
+		<div className={"text-left mt-[5px]"}>
+			<div
+				className={
+					"bg-primary1 text-black px-[20px] py-[10px] rounded-b-medium rounded-tr-medium inline-block"
+				}
+			>
+				{questions}
+			</div>
+		</div>
+	);
+}
+
+export default LastDialogbox;
