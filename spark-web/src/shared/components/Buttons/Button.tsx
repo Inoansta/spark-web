a6a89bc8e70f5f318ed1a646158934961324c409
Paul Yang <sea5122916@gmail.com>
2025-01-27 23:04:30 +0900

    fix: buttons컴포넌트 이름 변경

--- spark-web/src/shared/components/Buttons/Button.tsx
+++ spark-web/src/shared/components/Buttons/Button.tsx
@@ -1,4 +1,4 @@
-interface Buttons {
+interface Button {
   text: string;
   onClick: () => void;
   buttonType:
@@ -16,7 +16,7 @@ const style = {
     'px-[20px] w-full max-w-[335px] h-[48px] bg-primary5 text-white text-subtitle-b rounded-medium active:bg-primary7',
 };
 
-function Buttons({ text, onClick, buttonType }: Buttons) {
+function Button({ text, onClick, buttonType }: Button) {
   return (
     <div className={'flex justify-center'}>
       <button onClick={() => onClick()} className={style[buttonType]}>
@@ -26,4 +26,4 @@ function Buttons({ text, onClick, buttonType }: Buttons) {
   );
 }
 
-export default Buttons;
+export default Button;
