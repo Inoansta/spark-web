8b7a637c83f28da98e446caac0ce91adde704486
perfumeplaylist <perfumeplaylist@gmail.com>
2025-01-29 00:14:57 +0900

    feat: card 합성 컴포넌트 구현

---
+++ spark-web/src/shared/ui/components/Card.tsx
@@ -0,0 +1,49 @@
+import type { ReactNode } from 'react';
+import clsx from 'clsx';
+
+interface RootProps {
+  className?: string;
+  children: ReactNode;
+}
+
+function Root({ className, children }: RootProps) {
+  return (
+    <article
+      className={clsx(
+        `flex flex-col border rounded-2xl border-disabled gap-y-5`,
+        className,
+      )}
+    >
+      {children}
+    </article>
+  );
+}
+
+function Header({ className, children }: RootProps) {
+  return (
+    <div className={clsx(`flex flex-col px-5 pt-5 flex-1`, className)}>
+      {children}
+    </div>
+  );
+}
+
+function Content({ className, children }: RootProps) {
+  return (
+    <div
+      className={clsx(
+        `p-5 flex justify-between flex-2 gap-x-9 items-center`,
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Bottom({ className, children }: RootProps) {
+  return <div className={clsx(`flex-1`, className)}>{children}</div>;
+}
+
+const Card = Object.assign(Root, { Header, Content, Bottom });
+
+export default Card;
