1b2521673cb9585e709e7041459d6f7592b8ba76
Paul Yang <sea5122916@gmail.com>
2025-02-02 16:41:48 +0900

    feat: 분석 완료 안내 메세지 구현

---
+++ spark-web/src/domains/Results/components/ResultInteraction/index.tsx
@@ -0,0 +1,40 @@
+interface ResultInteraction {
+  channelName: string;
+}
+
+const animation = {
+  animationName: 'ResultInteraction',
+  animationDelay: '1200ms',
+  animationTimingFunction: 'ease-in-out',
+  animationDuration: '1500ms',
+  visibility: 'hidden',
+  lineHeight: '1.2',
+  animationFillMode: 'forwards',
+};
+
+export function ResultInteractionKeyframe() {
+  return (
+    <style>
+      {`
+      @keyframes ResultInteraction {
+        from { top: -20%; visibility: visible;}
+        to { top: 33%; visibility: visible;}
+      }
+    `}
+    </style>
+  );
+}
+
+export function ResultInteraction({ channelName }: ResultInteraction) {
+  return (
+    <div
+      className={
+        'text-white text-title3-eb px-[10px] py-[20px] text-center border border-white rounded-extraLarges bg-white bg-opacity-30 mx-[35px] absolute z-20'
+      }
+      style={animation}
+    >
+      {channelName}님의
+      <br /> 채널 분석이 완료 됐어요!
+    </div>
+  );
+}
