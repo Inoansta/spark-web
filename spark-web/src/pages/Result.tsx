33f1f98f7ebcfef124d2002af00f5ff6705ff4df
Paul Yang <sea5122916@gmail.com>
2025-02-02 16:42:35 +0900

    feat: 분석완료 페이지 구현

--- spark-web/src/pages/Result.tsx
+++ spark-web/src/pages/Result.tsx
@@ -1,3 +1,27 @@
+// import Lottie from 'react-lottie';
+import analysis from '@/assets/result/analaysis.json';
+import {
+  Footer,
+  ResultInteraction,
+  ResultInteractionKeyframe,
+} from '@/domains/Results/components';
+
+// const defaultOptions = {
+//   animationData: analysis,
+//   loop: true,
+// };
+
 export default function Result() {
-  return <div>Result</div>;
+  return (
+    <div>
+      <ResultInteractionKeyframe />
+      {/* <div className={'absolute z-10'}>
+        <Lottie options={defaultOptions} />
+      </div> */}
+      <div className={'flex justify-center'}>
+        <ResultInteraction channelName={'Spark'} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
