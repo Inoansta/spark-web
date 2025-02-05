fa20990bf4a7d7979b84d563ca55a6acaa11503f
Paul Yang <sea5122916@gmail.com>
2025-02-05 16:42:11 +0900

    feat: 아이콘 추가 및 수정

--- spark-web/src/assets/svg/nav/BackIcon.tsx
+++ spark-web/src/assets/svg/nav/BackIcon.tsx
@@ -1,6 +1,10 @@
 import { SvgProps } from './SettingIcon';
 
-export const BackIcon = ({ className, ...props }: SvgProps) => {
+export const BackIcon = ({
+  color = 'black',
+  className,
+  ...props
+}: SvgProps) => {
   return (
     <svg
       width="24"
@@ -13,7 +17,7 @@ export const BackIcon = ({ className, ...props }: SvgProps) => {
     >
       <path
         d="M8.55439 11.5271L15.1552 17.0659C15.5783 17.4209 15.6335 18.0517 15.2785 18.4747C14.9235 18.8978 14.2927 18.953 13.8696 18.598L6.36322 12.2993C6.30343 12.2492 6.25099 12.1935 6.20601 12.1337C5.88238 11.7115 5.9458 11.1052 6.35736 10.7599L13.8646 4.46054C14.2877 4.10554 14.9184 4.16072 15.2734 4.5838C15.6284 5.00687 15.5732 5.63763 15.1502 5.99263L8.55439 11.5271Z"
-        fill="#333333"
+        fill={color}
       />
     </svg>
   );
