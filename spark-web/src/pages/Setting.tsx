0b51d574ca9692a485e9198c699f443f293a4766
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-10 21:50:14 +0900

    feat: instaGram 아이콘 및 LocationMove 컴포넌트 추가, Setting 페이지에 통합

--- spark-web/src/pages/Setting.tsx
+++ spark-web/src/pages/Setting.tsx
@@ -1,3 +1,70 @@
+import { InstaGram } from '@/assets/svg/logo/InstaGram';
+import { BackIcon } from '@/assets/svg/nav/BackIcon';
+import LocationMove from '@/domains/Login/components/LocationMove';
+import { NavigationHeader } from '@/shared/components';
+import { Divider, Text } from '@/shared/ui';
+
+const LIST_DATA = [
+  { title: '앱 버전', description: '1.0.00', location: '/' },
+  { title: '공지사항', location: '/' },
+  { title: '문의하기', location: '/' },
+  { title: '서비스 이용약관', location: 'https://www.app-spark.privacy/' },
+  {
+    title: '개인정보처리방침',
+    location: 'https://www.app-spark.privacy/privacy',
+  },
+  {
+    title: '스파크 인스타',
+    description: '@spark__kr',
+    location: 'https://www.instagram.com/spark__kr/?igsh=b3VubXVtZnJjYmE%3D#',
+    rightIcon: InstaGram,
+    descriptionColor: 'primary3',
+  },
+];
+
 export default function Setting() {
-  return <div>setting</div>;
+  return (
+    <>
+      <NavigationHeader>
+        <NavigationHeader.LeftContent location="back">
+          <BackIcon />
+        </NavigationHeader.LeftContent>
+      </NavigationHeader>
+      <main>
+        <ul className="flex-col p-5 gap-x-[5px]">
+          {LIST_DATA.map(
+            ({
+              title,
+              description,
+              location,
+              rightIcon: RightIcon,
+              descriptionColor,
+            }) => (
+              <li key={title}>
+                <LocationMove
+                  className="flex justify-between px-5 py-3"
+                  location={location}
+                >
+                  <div
+                    className={`${RightIcon && 'flex items-center mr-[2px]'}`}
+                  >
+                    <Text as="title" title={title} />
+                    {RightIcon && <RightIcon />}
+                  </div>
+                  {description && (
+                    <Text
+                      as="description"
+                      title={description}
+                      className={`text-${descriptionColor}`}
+                    />
+                  )}
+                </LocationMove>
+                <Divider />
+              </li>
+            ),
+          )}
+        </ul>
+      </main>
+    </>
+  );
 }
