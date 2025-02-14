97531cee99476efea6c4a11c9dc46a3f9e209d35
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-15 03:57:15 +0900

    refactor: home 컴포넌트에서 HasToken 및 NotCardInfo를 HasTokenChannelInfo로 변경하고 Flex 컴포넌트로 구조 개선

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,36 @@
 import { Logo } from '@/assets/svg/logo/Logo';
 import { SmallLogo } from '@/assets/svg/logo/SmallLogo';
 import { FrontIcon } from '@/assets/svg/nav/FrontIcon';
-import { CardInfo, CheerupBar } from '@/domains/Home/components';
-import HasToken from '@/domains/Home/components/HasToken';
-import NotCardInfo from '@/domains/Home/components/NotCardInfo';
+import { CheerupBar, HasTokenChannelInfo } from '@/domains/Home/components';
 import { NavigationHeader } from '@/shared/components';
-import { Card, Spacing, Text } from '@/shared/ui';
-import SpeechBubble from '@/shared/ui/layout/SpeechBubble';
+import { Card, Flex, Spacing, SpeechBubble, Text } from '@/shared/ui';
 
 export default function Home() {
   return (
-    <main className="bg-line flex flex-col gap-y-5 min-h-full">
-      <NavigationHeader>
-        <NavigationHeader.LeftContent location="/">
-          <Logo />
-        </NavigationHeader.LeftContent>
-      </NavigationHeader>
-      <CheerupBar />
-      <div className="px-5 flex flex-col gap-y-5">
-        <HasToken fallback={<NotCardInfo />}>
-          <CardInfo />
-        </HasToken>
-        <div className="flex">
-          <SpeechBubble text="스파크가 궁금하다면?" icon={<SmallLogo />} />
-        </div>
-        <Card>
-          <Card.Header>
-            <div className="flex justify-between pb-5 items-center">
-              <Text as="title" title="이용 가이드" />
-              <FrontIcon />
-            </div>
-          </Card.Header>
-        </Card>
-        <Spacing size="large" />
-      </div>
+    <main className="bg-line min-h-full">
+      <Flex direction="column" gapY={5}>
+        <NavigationHeader>
+          <NavigationHeader.LeftContent location="/">
+            <Logo />
+          </NavigationHeader.LeftContent>
+        </NavigationHeader>
+        <CheerupBar />
+        <Flex direction="column" gapY={5} className="px-5">
+          <HasTokenChannelInfo />
+          <Flex>
+            <SpeechBubble text="스파크가 궁금하다면?" icon={<SmallLogo />} />
+          </Flex>
+          <Card className="pb-5">
+            <Card.Header>
+              <Flex justify="between" align="center">
+                <Text as="title" title="이용 가이드" />
+                <FrontIcon />
+              </Flex>
+            </Card.Header>
+          </Card>
+          <Spacing size="large" />
+        </Flex>
+      </Flex>
     </main>
   );
 }
