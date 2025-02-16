e969d2545202afcb8f477949cbb1e310ceb115b5
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-16 17:47:59 +0900

    fix: analysis 컴포넌트 구조 개선 및 SVG 아이콘 추가

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -1,16 +1,20 @@
 import analysis from '@/assets/animation/analysis/analysis.json';
-import loadingIcon from '@/assets/animation/analysis/loadingIcon.json';
+import { Analysis as AnalysisIcon } from '@/assets/svg/logo/Analysis';
+import { Bulb } from '@/assets/svg/logo/Bulb';
+import { Graph } from '@/assets/svg/logo/Graph';
 import { YoutubeIcon } from '@/assets/svg/logo/YoutubeIcon';
-import { LottieAnimation, Spacing, Text } from '@/shared/ui';
-// import Analysis from '@/assets/svg/logo/Analysis';
-// import Bulb from '@/assets/svg/logo/Bulb';
-// import Graph from '@/assets/svg/logo/Graph';
-// import successIcon from '@/assets/analysis/successIcon.json';
+import AnalysisState from '@/domains/Analysis/components/AnalysisState';
+import { Flex, LottieAnimation, Spacing, Text } from '@/shared/ui';
 
 export default function Analysis() {
   return (
     <main className="bg-line p-[10px]">
-      <section className="py-[10px] flex flex-col items-center flex-1">
+      <Flex
+        direction="column"
+        align="center"
+        className="flex-1 py-[10px]"
+        as="section"
+      >
         <Text
           as="title"
           title="조금만 기다려주세요"
@@ -21,28 +25,21 @@ export default function Analysis() {
           title="열심히 분석하고 있어요!"
           className="text-gray5"
         />
-      </section>
-      <section className="flex justify-center">
+      </Flex>
+      <Flex justify="center" as="section">
         <LottieAnimation
           animationData={analysis}
           className="w-[300px] h-[300px]"
         />
-      </section>
-      <Spacing size="xlsmall" />
-      <ul className="flex flex-col gap-y-5 px-5">
-        <li className="flex justify-between items-center px-[15px] py-[5px]">
-          <article className="flex">
-            <div className="bg-primary5 rounded-[20px] p-2 mr-[10px]">
-              <YoutubeIcon />
-            </div>
-            <section className="flex flex-col gap-x-[2px]">
-              <Text as="title" title="채널 정보 불러오기" />
-              <Text as="description" title="완료됐어요" />
-            </section>
-          </article>
-          <LottieAnimation animationData={loadingIcon} />
-        </li>
-      </ul>
+      </Flex>
+
+      <Flex direction="column" gapY={5} className="px-[50px]" as="ul">
+        <AnalysisState icon={<YoutubeIcon />} title="채널 정보 불러오기" />
+        <AnalysisState icon={<AnalysisIcon />} title="채널 데이터 분석" />
+        <AnalysisState icon={<Graph />} title="채널 정보 불러오기" />
+        <AnalysisState icon={<Bulb />} title="채널 성장 비법" />
+      </Flex>
+
       <Spacing size="xsmall" />
     </main>
   );
