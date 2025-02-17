e19fa2c68b925cceb267056f881e614a3f4ad82f
Paul Yang <sea5122916@gmail.com>
2025-02-18 00:55:58 +0900

    feat: 상세페이지 완료

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,3 +1,36 @@
+import {
+  FrequentlyAskedQuestions,
+  HowToGrow,
+  Recommendation,
+  TopPage,
+  WorriedAbout,
+} from '@/domains/Detail/components';
+import GraySpace from '@/domains/Detail/components/GraySpace';
+import { RouteMove } from '@/shared/components';
+
 export default function Detail() {
-  return <div>detail</div>;
+  return (
+    <div>
+      <TopPage />
+      <GraySpace />
+      <WorriedAbout />
+      <HowToGrow />
+      <GraySpace />
+      <Recommendation />
+      <GraySpace />
+      <FrequentlyAskedQuestions />
+      <RouteMove
+        location={'/user-info'}
+        className={'sticky bottom-0 flex justify-center mb-[30px]'}
+      >
+        <button
+          className={
+            'px-[20px] w-full max-w-[335px] h-[48px] bg-primary5 text-white text-subtitle-b rounded-medium active:bg-primary7'
+          }
+        >
+          시작하기
+        </button>
+      </RouteMove>
+    </div>
+  );
 }
