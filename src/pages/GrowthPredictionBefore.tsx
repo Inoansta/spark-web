6d4f87523783c6049e40a0de42434c6e8ca293f3
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-19 00:26:42 +0900

    feat: growthPredictionBefore 페이지 추가 및 라우터 설정

---
+++ src/pages/GrowthPredictionBefore.tsx
@@ -0,0 +1,59 @@
+// 로그인시에 닉네임 저장하기
+import { useEffect, useState } from 'react';
+import Before from '@/assets/animation/growthPrediction/growthPredictionBefore.mp4';
+import { BackIcon } from '@/assets/svg/nav/BackIcon';
+import { CloseIcon } from '@/assets/svg/nav/CloseIcon';
+import { NavigationHeader } from '@/shared/components';
+import { Flex } from '@/shared/ui';
+
+function DelayText({ delay = 7000 }: { delay?: number }) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  return (
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-full">
+      {visible && (
+        <Flex direction="column" justify="center" align="center">
+          <p className="text-white text-3xl font-bold drop-shadow-lg">
+            000님의
+          </p>
+          <p className="text-white text-3xl font-bold drop-shadow-lg">
+            채널 성장은?
+          </p>
+        </Flex>
+      )}
+    </div>
+  );
+}
+
+function VideoPlayer() {
+  return (
+    <div className="relative w-full h-screen">
+      <NavigationHeader className={'p-5 max-w-[450px] w-full absolute top-4'}>
+        <NavigationHeader.LeftContent location="back">
+          <BackIcon />
+        </NavigationHeader.LeftContent>
+        <NavigationHeader.RightContent location="/detail">
+          <CloseIcon />
+        </NavigationHeader.RightContent>
+      </NavigationHeader>
+      <video className="w-full h-full object-cover" autoPlay muted playsInline>
+        <source src={Before} type="video/mp4" />
+        브라우저가 비디오를 지원하지 않습니다.
+      </video>
+
+      <DelayText />
+    </div>
+  );
+}
+
+export default function GrowthPredictionBefore() {
+  return <VideoPlayer />;
+}
