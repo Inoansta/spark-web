9785bdad52d730c905082cb3f364b04643f98415
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-18 15:37:58 +0900

    fix: text 컴포넌트에 card_description 스타일 추가

--- src/shared/ui/components/Text.tsx
+++ src/shared/ui/components/Text.tsx
@@ -10,6 +10,7 @@ interface TextProps {
 const style = {
   title: 'text-black font-extrabold text-[15px] not-italic',
   description: 'text-gray font-medium text-[13px] not-italic',
+  card_description: 'text-xs font-bo ld text-white not-italic',
 } as const;
 
 export default function Text({ as, title, className }: TextProps) {
