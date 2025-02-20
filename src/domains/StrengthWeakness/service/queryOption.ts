4c1fe550cb73d0ca86f98d531ce8d25b1af2ee6a
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-20 16:26:42 +0900

    feat: result 인터페이스를 export로 변경하고 strengths 및 weaknesses 타입을 수정하여 데이터 변환 로직 추가

--- src/domains/StrengthWeakness/service/queryOption.ts
+++ src/domains/StrengthWeakness/service/queryOption.ts
@@ -1,5 +1,10 @@
 import { queryOptions } from '@tanstack/react-query';
+import { Result } from '../model/type';
 import strengthWeakApi from './handler';
+
+const dateLabels = ['90 ~ 60일', '60 ~ 30일', '30 ~ 최근'];
+const strengthsColor = ['bg-gradient8', 'bg-primary10', 'bg-primary11'];
+const weaknessesColor = ['bg-gradient5', 'bg-gradient4', 'bg-gradient3'];
 
 const strengthWeakQueryOption = {
   stats: ['stats'] as const,
@@ -7,6 +12,32 @@ const strengthWeakQueryOption = {
     queryOptions({
       queryKey: [...strengthWeakQueryOption.stats, 'strengthWeak', channelId],
       queryFn: () => strengthWeakApi.getStrengthWeakApi({ channelId }),
+      select: (data) => {
+        const { stats, strengths, weaknesses } = data.result;
+
+        const transformData = (
+          title: 'strengths' | 'weaknesses',
+          categories: Result['weaknesses'],
+        ) => {
+          return categories.map((category) =>
+            stats.map((stat, index) => ({
+              date: dateLabels[index],
+              color:
+                title === 'strengths'
+                  ? strengthsColor[index]
+                  : weaknessesColor[index],
+              value: stat[category],
+            })),
+          );
+        };
+
+        const transformedData = {
+          strengths: transformData('strengths', strengths),
+          weaknesses: transformData('weaknesses', weaknesses),
+        };
+
+        return { transformedData, weaknesses };
+      },
     }),
 };
 
