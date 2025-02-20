c53644996dd0d062590a903bcad6ddc77247c77e
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-20 12:52:17 +0900

    feat: zustand을 사용하여 전략 저장소 추가 및 의존성 업데이트

---
+++ src/app/store/useStrategyStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+import { combine } from 'zustand/middleware';
+
+const initialState = {
+  channelId: '',
+  userContents: '',
+  userFulltime: '',
+  userGoal: '',
+  weaknesses: [''],
+};
+
+const useStrategyStore = create(
+  combine(initialState, (set) => ({
+    setField: (
+      field: keyof typeof initialState,
+      value: (typeof initialState)[keyof typeof initialState],
+    ) => set({ [field]: value }),
+  })),
+);
+
+export default useStrategyStore;
