aff42badef7c9f369f15d7a544aae34544d8f2c3
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-20 14:20:41 +0900

    feat: mSW를 사용하여 모의 API 요청 처리 및 서비스 워커 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,28 @@ import { Provider } from './app/provider';
 import { RouterApp } from './app/router';
 import { GlobalLayout } from './shared/ui';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <GlobalLayout>
-      <Provider>
-        {/* <QuerySuspenseBoundary> */}
-        <RouterApp />
-        {/* </QuerySuspenseBoundary> */}
-      </Provider>
-    </GlobalLayout>
-  </StrictMode>,
+async function deferRender() {
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+deferRender().then(() =>
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <GlobalLayout>
+        <Provider>
+          {/* <QuerySuspenseBoundary> */}
+          <RouterApp />
+          {/* </QuerySuspenseBoundary> */}
+        </Provider>
+      </GlobalLayout>
+    </StrictMode>,
+  ),
 );
