31c966dc391e929cec8dc7f364dfa4c773354f58
Paul Yang <sea5122916@gmail.com>
2025-02-21 11:13:09 +0900

    feat: requestid 전용 store 추가

---
+++ src/pages/StrategyBefore.tsx
@@ -0,0 +1,26 @@
+import strategyLottie from '@/assets/animation/strategyBefore.json';
+import { LottieAnimation } from '@/shared/ui';
+
+export default function StrategyBefore() {
+  return (
+    <div
+      className={
+        'bg-gradient-to-b from-black to-[#FBB1F5] via-[#4557FF] pt-[44px] flex flex-col justify-center items-center h-screen'
+      }
+    >
+      <div className={'px-[40px] mb-[30px]'}>
+        <LottieAnimation animationData={strategyLottie} />
+      </div>
+      <div className={'flex flex-col gap-[10px]'}>
+        <div className={'text-title5-b text-center text-white leading-[26px]'}>
+          잠시만요!
+          <br />
+          AI가 비법을 찾고 있어요
+        </div>
+        <div className={'text-body2-m text-center text-white'}>
+          예상 소요시간 20초
+        </div>
+      </div>
+    </div>
+  );
+}
