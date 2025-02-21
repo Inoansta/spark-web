785267ec19a687e64181217a24a2936a72ebab2a
Paul Yang <sea5122916@gmail.com>
2025-02-21 11:22:36 +0900

    feat: 인기페이지 top3 api 연결

--- src/pages/Popular.tsx
+++ src/pages/Popular.tsx
@@ -1,121 +1,10 @@
 import { PopularCard } from '@/domains/Popular/components';
+import usePopularQuery from '@/domains/Popular/hooks/usePopularQuery';
 import { NextPageFooter } from '@/shared/components';
 
-const dummy = [
-  {
-    id: 'bIjKtpOw0II',
-    snippet: {
-      title: '20211025 기디비',
-      publishedAt: '2021-11-01T21:48:27Z',
-      thumbnails: {
-        default: {
-          url: 'https://i.ytimg.com/vi/bIjKtpOw0II/default.jpg',
-          width: 120,
-          height: 90,
-        },
-        medium: {
-          url: 'https://i.ytimg.com/vi/bIjKtpOw0II/mqdefault.jpg',
-          width: 320,
-          height: 180,
-        },
-        high: {
-          url: 'https://i.ytimg.com/vi/bIjKtpOw0II/hqdefault.jpg',
-          width: 480,
-          height: 360,
-        },
-        standard: {
-          url: 'https://i.ytimg.com/vi/bIjKtpOw0II/sddefault.jpg',
-          width: 640,
-          height: 480,
-        },
-        maxres: {
-          url: 'https://i.ytimg.com/vi/bIjKtpOw0II/maxresdefault.jpg',
-          width: 1280,
-          height: 720,
-        },
-      },
-    },
-    statistics: {
-      viewCount: 17,
-    },
-  },
-  {
-    id: 'ZCsy2E8vWFE',
-    snippet: {
-      title: '20210923 대예이1교시',
-      publishedAt: '2021-09-25T07:25:48Z',
-      thumbnails: {
-        default: {
-          url: 'https://i.ytimg.com/vi/ZCsy2E8vWFE/default.jpg',
-          width: 120,
-          height: 90,
-        },
-        medium: {
-          url: 'https://i.ytimg.com/vi/ZCsy2E8vWFE/mqdefault.jpg',
-          width: 320,
-          height: 180,
-        },
-        high: {
-          url: 'https://i.ytimg.com/vi/ZCsy2E8vWFE/hqdefault.jpg',
-          width: 480,
-          height: 360,
-        },
-        standard: {
-          url: 'https://i.ytimg.com/vi/ZCsy2E8vWFE/sddefault.jpg',
-          width: 640,
-          height: 480,
-        },
-        maxres: {
-          url: 'https://i.ytimg.com/vi/ZCsy2E8vWFE/maxresdefault.jpg',
-          width: 1280,
-          height: 720,
-        },
-      },
-    },
-    statistics: {
-      viewCount: 16,
-    },
-  },
-  {
-    id: 'wWizg_GYeJc',
-    snippet: {
-      title: '20211022 기디비',
-      publishedAt: '2021-10-28T20:48:44Z',
-      thumbnails: {
-        default: {
-          url: 'https://i.ytimg.com/vi/wWizg_GYeJc/default.jpg',
-          width: 120,
-          height: 90,
-        },
-        medium: {
-          url: 'https://i.ytimg.com/vi/wWizg_GYeJc/mqdefault.jpg',
-          width: 320,
-          height: 180,
-        },
-        high: {
-          url: 'https://i.ytimg.com/vi/wWizg_GYeJc/hqdefault.jpg',
-          width: 480,
-          height: 360,
-        },
-        standard: {
-          url: 'https://i.ytimg.com/vi/wWizg_GYeJc/sddefault.jpg',
-          width: 640,
-          height: 480,
-        },
-        maxres: {
-          url: 'https://i.ytimg.com/vi/wWizg_GYeJc/maxresdefault.jpg',
-          width: 1280,
-          height: 720,
-        },
-      },
-    },
-    statistics: {
-      viewCount: 13,
-    },
-  },
-];
-
 export default function Popular() {
+  const popular = usePopularQuery();
+
   return (
     <div>
       <div
@@ -132,10 +21,10 @@ export default function Popular() {
         <div className={'flex flex-1 flex-col'}>
           <div
             className={
-              'flex  gap-[20px] flex-col mt-[calc(100vh/6)] mb-[100px]'
+              'flex  gap-[20px] flex-col mt-[calc(100vh/10)] mb-[100px]'
             }
           >
-            {dummy.map((item, index) => (
+            {popular.data.result.map((item, index) => (
               <PopularCard
                 videoInformation={item}
                 index={index}
