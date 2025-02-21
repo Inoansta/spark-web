bce57e7fb1323e854839e4e23ff5f29469f4a643
Paul Yang <sea5122916@gmail.com>
2025-02-21 16:58:22 +0900

    fix: 오류 접근 수정

--- src/domains/Strategy/service/handler.ts
+++ src/domains/Strategy/service/handler.ts
@@ -17,12 +17,12 @@ const strategyApi = {
       snsGoal,
       weaknesses,
     };
-    console.log('postStrategy body', body);
+
     return httpClient.post<ResponseStrategy, typeof body>(url, body);
   },
   getStrategy: async ({ requestId }: { requestId: string }) => {
     const url = `${BASE_URL}/strategy/${requestId}`;
-    console.log(url);
+
     const response = await httpClient.get(url);
     return response;
   },
