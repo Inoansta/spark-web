2ba05ba74a80a7735884f8c02bd1624720977a44
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-24 09:03:03 +0900

    fix: growthPredictionBefore.mp4 비디오 파일 추가 및 VideoPlayer 컴포넌트에서 경로 수정

--- src/domains/GrowthPrediction/components/VideoPlayer.tsx
+++ src/domains/GrowthPrediction/components/VideoPlayer.tsx
@@ -1,11 +1,10 @@
-import Before from '@/assets/animation/growthPrediction/growthPredictionBefore.mp4';
 import DelayText from './DelayText';
 
 export default function VideoPlayer() {
   return (
     <div className="relative w-full h-screen">
       <video className="w-full h-full object-cover" autoPlay muted playsInline>
-        <source src={Before} type="video/mp4" />
+        <source src="/videos/growthPredictionBefore.mp4" type="video/mp4" />
         브라우저가 비디오를 지원하지 않습니다.
       </video>
 
