4b784d89e9d5af114689c06c61c198532028c4fb
perfumeplaylist <perfumeplaylist@gmail.com>
2025-03-03 17:01:03 +0900

    feat: 쿼리 서스펜스 경계 추가 및 페이지 배경 컴포넌트 적용

--- src/pages/StrengthWeakness.tsx
+++ src/pages/StrengthWeakness.tsx
@@ -1,10 +1,13 @@
-import { Suspense } from 'react';
+import { QuerySuspenseBoundary } from '@/app/provider';
 import { StrengthWeaknessFunnel } from '@/domains/StrengthWeakness';
+import PageBackground from '@/shared/ui/components/PageBackground';
 
 export default function StrengthWeakness() {
   return (
-    <Suspense fallback={<>Loading...</>}>
-      <StrengthWeaknessFunnel />
-    </Suspense>
+    <PageBackground color="black_gradient">
+      <QuerySuspenseBoundary loadingFallback={<>Loading...</>}>
+        <StrengthWeaknessFunnel />
+      </QuerySuspenseBoundary>
+    </PageBackground>
   );
 }
