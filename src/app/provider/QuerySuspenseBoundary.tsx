85146ca020a7bae8e2592bf41da34487989bae58
perfumeplaylist <perfumeplaylist@gmail.com>
2025-03-03 16:48:34 +0900

    feat: 네트워크 오류 처리 컴포넌트 추가 및 에러 아이콘 이미지 추가

--- src/app/provider/QuerySuspenseBoundary.tsx
+++ src/app/provider/QuerySuspenseBoundary.tsx
@@ -1,14 +1,14 @@
 import { type PropsWithChildren, type ReactNode, Suspense } from 'react';
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
+import ErrorIcon from '@/assets/ErrorIcon.png';
 import { Flex } from '@/shared/ui';
-
 interface QuerySuspenseBoundaryProps extends PropsWithChildren {
   ErrorFallback?: ReactNode;
   loadingFallback: ReactNode;
 }
 
-const ErrorComponents = ({ resetErrorBoundary }: FallbackProps) => {
+const NetWorkErrorComponent = ({ resetErrorBoundary }: FallbackProps) => {
   return (
     <Flex
       justify="center"
@@ -24,6 +24,28 @@ const ErrorComponents = ({ resetErrorBoundary }: FallbackProps) => {
         className="p-4 bg-primary6 text-white rounded-lg"
       >
         다시 시도
+      </button>
+    </Flex>
+  );
+};
+
+const ErrorComponents = ({ resetErrorBoundary, ...props }: FallbackProps) => {
+  console.log(props);
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      direction="column"
+      gapY={5}
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+    >
+      <img src={ErrorIcon} alt="error_image" />
+      <p className="">정보를 불러올 수 없어요.</p>
+      <button
+        onClick={resetErrorBoundary}
+        className="px-5 py-[10px] bg-primary5 text-white rounded-[10px]"
+      >
+        다시 불러오기
       </button>
     </Flex>
   );
