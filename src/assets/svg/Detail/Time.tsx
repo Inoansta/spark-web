dc006a9cc8bcb2e7f816d91a1836d3f5fe6f3882
JungHO014 <kjh@deep-medi.com>
2025-05-30 00:20:59 +0900

    feat: svgProps를 사용하여 Comment, Dollar, AddIcon, Time 컴포넌트에 fill 속성 추가 및 props 전파 기능 구현

--- src/assets/svg/Detail/Time.tsx
+++ src/assets/svg/Detail/Time.tsx
@@ -1,16 +1,16 @@
 import { SvgProps } from '../nav/UnifiedSettingIcon';
 
-export default function Time({ className }: SvgProps) {
+export default function Time({ fill = 'white', ...props }: SvgProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="32"
       height="33"
       fill="none"
-      className={className}
+      {...props}
     >
       <path
-        fill="#787CFE"
+        fill={fill}
         d="M16 3.167c7.364 0 13.333 5.969 13.333 13.333S23.364 29.833 16 29.833 2.667 23.863 2.667 16.5C2.667 9.136 8.636 3.166 16 3.166ZM16 8.5a1.333 1.333 0 0 0-1.333 1.333V16.5c0 .353.14.692.39.942l4 4a1.333 1.333 0 0 0 1.886-1.885l-3.61-3.61V9.834A1.333 1.333 0 0 0 16 8.5Z"
       />
     </svg>
