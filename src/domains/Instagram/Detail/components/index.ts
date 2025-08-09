625567fa290b01afdd9a9f1ddbdb5c02481457d3
Paul Yang <sea5122916@gmail.com>
2025-08-09 21:55:01 +0900

    feat: 배럴 형식 index추가

---
+++ src/domains/Instagram/Detail/components/index.ts
@@ -0,0 +1,4 @@
+export { default as InstagramFAQ } from './InstagramFAQ';
+export { default as InstagramRecommendation } from './InstagramRecommendation';
+export { default as InstagramKakao } from './InstagramKakao';
+export { default as WrongWay } from './WrongWay';
