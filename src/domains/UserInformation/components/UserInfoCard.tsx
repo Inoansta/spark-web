7866c4474eabd4f320fcac6f6a8b06b653ddf107
Paul Yang <sea5122916@gmail.com>
2025-09-16 18:29:23 +0900

    fix: 사용자 정보 페이지 에러 수정

---
+++ src/domains/UserInformation/components/UserInfoCard.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import { HomeYoutubeLogo } from '@/assets/svg/HomeYoutubeLogo';
+import { ChannelCommonCard } from '@/domains/Home/components/ChannelCommonCard';
+import { useChannelProfile, useMetaProfile } from '@/domains/Home/hooks';
+import {
+  formatNumberWithCommas,
+  formatNumberWithUnit,
+} from '@/domains/Home/lib/utils';
+import { TOKEN } from '@/domains/Login/hooks/useAuthToken';
+import { Storage } from '@/shared/lib';
+import { Flex, Text } from '@/shared/ui';
+
+function MetaCard() {
+  const { data } = useMetaProfile();
+  return (
+    <ChannelCommonCard
+      size="medium"
+      isLogin
+      avatarUrl={data.result.profileUrl}
+      header={
+        <Flex justify="between" align="center">
+          <Text
+            as="title"
+            title={`@${data.result.userName}`}
+            className="text-[13px]"
+          />
+        </Flex>
+      }
+      posting={formatNumberWithCommas(data.result.postsCount ?? 0)}
+      subscriber={formatNumberWithUnit(data.result.followersCount ?? 0, '명')}
+      totalView={formatNumberWithUnit(data.result.followingCount ?? 0, '명')}
+    />
+  );
+}
+
+function YoutubeCard() {
+  const { data } = useChannelProfile();
+  return (
+    <ChannelCommonCard
+      size="medium"
+      isLogin
+      avatarUrl={data.result.defaultThumbnailUrl}
+      header={
+        <Flex justify="between" align="center">
+          <Text
+            as="title"
+            title={`@${data.result.channelName}`}
+            className="text-[13px]"
+          />
+          <HomeYoutubeLogo className="mr-1" />
+        </Flex>
+      }
+      posting={formatNumberWithCommas(data.result.totalVideoCount ?? 0)}
+      subscriber={formatNumberWithUnit(data.result.subscriberCount ?? 0, '명')}
+      totalView={formatNumberWithUnit(data.result.totalViewCount ?? 0, '회')}
+    />
+  );
+}
+
+export default function UserInfoCard() {
+  const [refresh_token, setRefreshToken] = useState('');
+
+  useEffect(() => {
+    setRefreshToken(Storage.getLocalStorage(TOKEN.REFRESH));
+  }, []);
+
+  return refresh_token ? <YoutubeCard /> : <MetaCard />;
+}
