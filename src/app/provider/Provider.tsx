7b568d1c3f637e61d5a96920c8cbf8e91818c8a6
Paul Yang <sea5122916@gmail.com>
2025-12-06 20:47:09 +0900

    fix: queryclient 생성 방식 수정

--- src/app/provider/Provider.tsx
+++ src/app/provider/Provider.tsx
@@ -1,21 +1,18 @@
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function QueryProvider({ children }: PropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 60 * 5,
-            gcTime: 1000 * 60 * 10,
-            retry: 0,
-            throwOnError: true,
-          },
-        },
-      }),
-  );
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      retry: 0,
+      throwOnError: true,
+    },
+  },
+});
 
+export default function QueryProvider({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
